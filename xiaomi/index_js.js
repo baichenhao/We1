ae016ce95ef15766e285f593f9e33bd5cb3eed68
baichenhao <1441448530@qq.com>
2017-11-19 21:49:34 +0800

    小米官网仿写

---
+++ xiaomi/index_js.js
@@ -0,0 +1,84 @@
+function fudong1 () {
+	var p1 = document.getElementById("p1");
+	p1.id='d1';
+}
+function out1 () {
+	var p1 = document.getElementById("d1");
+	p1.id='p1';
+}
+function fudong2 () {
+	var p1 = document.getElementById("k1");
+	var k_1= document.getElementById("k_1");
+	p1.id='d2';
+	k_1.id='d_1';
+}
+function out2 () {
+	var p1 = document.getElementById("d2");
+	var k_1= document.getElementById("d_1");
+	p1.id='k1';
+	k_1.id='k_1';
+}
+function fudong3 () {
+	var p1 = document.getElementById("k2");
+	p1.id='d3';
+}
+function out3 () {
+	var p1 = document.getElementById("d3");
+	p1.id='k2';
+}
+function fudong4 () {
+	var p1 = document.getElementById("k3");
+	p1.id='d4';
+}
+function out4 () {
+	var p1 = document.getElementById("d4");
+	p1.id='k3';
+}
+function fudong5 () {
+	var p1 = document.getElementById("k4");
+	p1.id='d5';
+}
+function out5 () {
+	var p1 = document.getElementById("d5");
+	p1.id='k4';
+}
+function fudong6 () {
+	var p1 = document.getElementById("k5");
+	p1.id='d6';
+}
+function out6 () {
+	var p1 = document.getElementById("d6");
+	p1.id='k5';
+}
+function fudong7 () {
+	var p1 = document.getElementById("k6");
+	p1.id='d7';
+}
+function out7 () {
+	var p1 = document.getElementById("d7");
+	p1.id='k6';
+}
+function fudong8 () {
+	var p1 = document.getElementById("k7");
+	p1.id='d8';
+}
+function out8 () {
+	var p1 = document.getElementById("d8");
+	p1.id='k7';
+}
+function fudong9 () {
+	var p1 = document.getElementById("k8");
+	p1.id='d9';
+}
+function out9 () {
+	var p1 = document.getElementById("d9");
+	p1.id='k8';
+}
+function fudong10 () {
+	var p1 = document.getElementById("k9");
+	p1.id='d10';
+}
+function out10 () {
+	var p1 = document.getElementById("d10");
+	p1.id='k9';
+}
