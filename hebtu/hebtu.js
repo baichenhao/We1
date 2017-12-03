e68dee10d384b80eead86ce260934b4e7b218f7e
baichenhao <1441448530@qq.com>
2017-12-03 22:39:00 +0800

    hebtu

---
+++ hebtu/hebtu.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+			var liNodes = document.getElementsByTagName("li");
+			for (var i=0; i<liNodes.length; i++)
+			{
+				liNodes[i].onmouseover = function(){
+					this.className = "subMenuShow";
+				}
+				liNodes[i].onmouseout = function(){
+					this.className = "";
+				}
+			}
+		}
+var wrap = document.getElementsByClassName('')
